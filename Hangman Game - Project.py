b7f19fb091bb62d7a85cf9c15c321da59b0b42ff
Sead Abduli <abdulisead@icloud.com>
2025-12-17 18:52:48 +0100

    Update Hangman Game - Project.py

--- Hangman Game - Project.py
+++ Hangman Game - Project.py
@@ -1,5 +1,15 @@
 import random
-
+print("**** Welcome to Hangman Game ****"
+      
+    " _\n"
+    "| |\n"
+    "| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __\n"
+    "| '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\\n"
+    "| | | | (_| | | | | (_| | | | | | | (_| | | | |\n"
+    "|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|\n"
+    "                     __/ |\n"
+    "                    |___/\n"
+)
 stages = [r'''
   +---+
   |   |
@@ -66,6 +76,8 @@ display_word = random_word
 for char in random_word:
     display_word = display_word.replace(char, "_")
 
+print("The random word has been chosen!")
+
 print(display_word)
 
 word_underline = list(display_word)
@@ -74,7 +86,7 @@ maximum_guesses = 7
 
 while "".join(word_underline) != random_word and maximum_guesses > 0:
 
-    guess = input("Guess a letter: ").lower()
+    guess = input("Guess a letter to fill the empty brackets:  ").lower()
 
     if guess in random_word:
 
@@ -93,5 +105,5 @@ while "".join(word_underline) != random_word and maximum_guesses > 0:
         print(f"Congratulations, you guessed the right word.The word to be guessed was: {random_word}!")
 
     elif maximum_guesses <= 0:
-        print("Sorry, you didn't guess the word! You lost!")
+        print(f"Sorry, you didn't guess the word! You lost! The word to be guessed was: {random_word}!")
         break
